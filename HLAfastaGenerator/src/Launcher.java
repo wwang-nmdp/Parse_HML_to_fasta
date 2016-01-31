8cc7b4e45ec5f2837731d4451318ebddb10a1953
Wei Wang <wwang@NMDP.ORG>
2016-01-31 13:38:02 -0600

    import code.

---
+++ HLAfastaGenerator/src/Launcher.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+public class Launcher {
+	private static FastaGenerator generetor;
+
+	public static void main(String[] args) {
+	
+		generetor = new FastaGenerator();
+		File folder = new File("./HLAfastaGenerator/input");
+		File outputFolder = new File("./HLAfastaGenerator/output");
+		try {
+			FileUtils.cleanDirectory(outputFolder);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		File[] inputList = folder.listFiles();
+		for(int i = 0 ; i < inputList.length; i++){
+			if(inputList[i].getName().contains("xml")){
+				String fileName = FilenameUtils.removeExtension(inputList[i].getName());
+				process(inputList[i], fileName);
+			}
+			
+		}
+
+	}
+	
+	private static void process(File input, String outputName){
+		
+		File output = new File("./HLAfastaGenerator/output/" + outputName +".fasta");
+		try{
+			generetor.run(input,output);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
